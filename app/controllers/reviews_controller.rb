04087c2030bd5cac16e21fd92710f5fecef152de
Julia Pearson <julia.pearson@gmail.com>
2015-12-15 14:59:53 -0500

    controlling everything

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,4 +7,9 @@ class ReviewsController < ApplicationController
   def create 
     @review = Review.create({review_params})
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:name, :city, :state, :zip, :img_url)
+    end
 end	
