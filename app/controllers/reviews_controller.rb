125d7b7cf260a0b4faa43fa4688d17f08aff3293
Julia Pearson <julia.pearson@gmail.com>
2015-12-15 14:30:37 -0500

    welcome controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,10 @@
 class ReviewsController < ApplicationController::Base
   # no need for index or show here!! Reviews are only displayed on Listing page
+  def new
+    @review = Review.new
+  end
+
+  def create 
+    @review = Review.create({review_params})
+  end
 end	
