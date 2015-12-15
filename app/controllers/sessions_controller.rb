eb79330225016699aca346465887a0ee45bcf6c3
Alexander Schofield <alschofield93@gmail.com>
2015-12-15 15:13:17 -0500

    actually fixes login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: user_params[:email]).try(authenticate: user_params[:password])
+    @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
     if @user
       session[:user_id] = @user.id
       redirect_to user_path(@user)
@@ -18,10 +18,5 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to '/'
   end
-
-  private 
-    def user_params
-      params.require(:user).permit(:email, :password)
-    end
 
 end
