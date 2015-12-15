5f8294b5d9f34f938e915d85038a3a3ae6e2baa0
Julia Pearson <julia.pearson@gmail.com>
2015-12-15 12:03:41 -0500

    starting neighborhood new

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -2,7 +2,15 @@ class NeighborhoodsController < ActionController::Base
   def index
     @neighborhoods = Neighborhood.all
   end
+
   def show
     @neighborhood = Neighborhood.find(params[:id])
   end
+
+  def new
+    @neighborhood = Neighborhood.new
+  end
+
+  def create
+    @Neighborhood = Neighborhood.create({name: params[:name], city: params[:city], state:, name: params[:name] params[:state], zip: params[:zip]})
 end	
